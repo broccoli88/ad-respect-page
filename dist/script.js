54dfbbd2e251ccca39fe7275188714bed1b91c9f
KiAi <94138724+broccoli88@users.noreply.github.com>
2023-08-15 20:45:01 +0200

    nav-bar

---
+++ dist/script.js
@@ -0,0 +1,30 @@
+// NAVBR
+
+const menuButton = document.querySelector(".menu-icon")
+const menuShapes = document.querySelectorAll('.menu-icon__shape')
+const nav = document.querySelector('.nav')
+const inputIcon = document.querySelector('.list-item__input-icon')
+const inputListItem = document.querySelector('.list-item__input')
+const inputField = document.querySelector('.list-item__input-field')
+
+
+menuButton.addEventListener('click', () => {
+    menuShapes.forEach(shape => {
+        shape.classList.toggle('menu-icon--transform')
+
+    })
+    nav.classList.toggle('nav--show')
+})
+
+
+inputIcon.addEventListener('click', () => {
+    inputListItem.classList.add('list-item__input--visible')
+    inputField.classList.add('list-item__input-field--visible')
+    inputField.focus()
+})
+
+inputField.addEventListener('focusout', () => {
+    inputField.classList.remove('list-item__input-field--visible')
+
+    inputListItem.classList.remove('list-item__input--visible')
+})
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./dist/**/*.{html,js}"],
+    theme: {
+        colors: {
+            "black": "#000000",
+            "white": "#FFFFFF",
+            "beige": "#DCC1AB",
+            "gray": "#F5F0EC",
+            "green": "#1B5B31",
+        },
+        extend: {},
+    },
+    plugins: [],
+}
+
