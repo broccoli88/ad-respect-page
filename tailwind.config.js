9d6ed405b14517240df5aeb066b984cf5c911cb3
KiAi <94138724+broccoli88@users.noreply.github.com>
2023-08-17 20:00:49 +0200

    instagram section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
     content: ["./dist/**/*.{html,js}"],
     theme: {
+        fontFamily: {
+            'inter': ["Inter", 'sans-serif'],
+            'montserrat': ["Montserrat", 'sans-serif']
+        },
         colors: {
             "transparent": "transparent",
             "black": "#000000",
